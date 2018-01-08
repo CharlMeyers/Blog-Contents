b2f16de3d0a89e29f2b702ea63185abd3ba86294
Charl <meyers.charl@gmail.com>
2018-01-08 18:40:23 +0200

    Add browser client info data object

---
+++ Image Resize/ImageResizeDemo/ImageResizeDemo/Models/BrowserClientInfoDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImageResizeDemo.Models
+{
+    public class BrowserClientInfoDTO
+    {
+        public Guid BrowserClientID { get; set; }
+        public string UserAgentString { get; set; }
+        public int DisplayResolutionHeight { get; set; }
+        public int DisplayResolutionWidth { get; set; }
+    }
+}
