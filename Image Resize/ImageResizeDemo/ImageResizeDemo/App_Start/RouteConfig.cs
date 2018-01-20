a1a6d679dd7e769f59e6a442d77283d9f740c99f
Charl <meyers.charl@gmail.com>
2018-01-20 14:00:10 +0200

    Accept dot in request parameter

--- Image Resize/ImageResizeDemo/ImageResizeDemo/App_Start/RouteConfig.cs
+++ Image Resize/ImageResizeDemo/ImageResizeDemo/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace ImageResizeDemo
@@ -17,12 +13,6 @@ namespace ImageResizeDemo
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "BrowserCheck", action = "Index", id = UrlParameter.Optional }
-            );
-
-            routes.MapRoute(
-                name: "image",
-                url: "{controller}/{id}",
-                defaults: new { controller = "Image", id = UrlParameter.Optional }
             );
         }
     }
