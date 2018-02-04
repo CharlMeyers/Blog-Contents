246e021d3586f0dfeab301ca1e88d96254251420
Charl <meyers.charl@gmail.com>
2018-02-04 16:52:39 +0200

    Remove route debugger

--- Image Resize/ImageResizeDemo/ImageResizeDemo/App/profile.js
+++ Image Resize/ImageResizeDemo/ImageResizeDemo/App/profile.js
@@ -4,8 +4,10 @@
         var imageHeight = $(self).css("height").replace("px", "");
         var imageWidth = $(self).css("width").replace("px", "");
 
-        $.get("api/image/GetProfilePictureBase64/", { id: '20e49fb4-597d-4db0-b45a-781875ea35d9', height: imageHeight, width: imageWidth }, function (data) {
-            $(self).attr("src", "data:image/jpg;base64," + data);
+        $.get("api/image/GetProfilePictureBase64/",
+            { id: '20e49fb4-597d-4db0-b45a-781875ea35d9', height: imageHeight, width: imageWidth },
+            function (data) {
+                $(self).attr("src", "data:image/jpg;base64," + data);
         })
     });
 });
